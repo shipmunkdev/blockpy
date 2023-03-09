ad8584511a26e0d481e7950b20a9b3008a8c1330
Sein Tun <contact@seintun.dev>
2023-03-08 16:35:15 -0800

    Added sorting to ensure hash remains consistent regardless of the order of given args

--- crypto_hash.py
+++ crypto_hash.py
@@ -5,7 +5,7 @@ def crypto_hash(*args):
     """
     Return a sha-256 hash of the given arguments.
     """
-    stringified_args = map(lambda data: json.dumps(data), args)
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
     joined_data = ''.join(stringified_args)
 
     return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
@@ -13,6 +13,7 @@ def crypto_hash(*args):
 
 def main():
     print(f"crypto_hash('one', 2, [3]): {crypto_hash('one', 2, [3])}")
+    print(f"crypto_hash(2, 'one', [3]): {crypto_hash(2, 'one', [3])}")
 
 
 if __name__ == '__main__':
