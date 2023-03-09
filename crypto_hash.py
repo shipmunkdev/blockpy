ec81138b1e095ef23e71b7a11c32406e1c6441bf
Sein Tun <contact@seintun.dev>
2023-03-08 16:34:36 -0800

    Added encode/decode with lambda and *args to accept multiple arguments for hashing and stringfy

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,17 +1,18 @@
 import hashlib
 import json
 
+def crypto_hash(*args):
+    """
+    Return a sha-256 hash of the given arguments.
+    """
+    stringified_args = map(lambda data: json.dumps(data), args)
+    joined_data = ''.join(stringified_args)
 
-def crypto_hash(data):
-    """
-    Return a sha-256 hash of the given data.
-    """
-    stringified_data = json.dumps(data)
-    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 
 def main():
-    print(f"crypto_hash('foo'): {crypto_hash('foo')}")
+    print(f"crypto_hash('one', 2, [3]): {crypto_hash('one', 2, [3])}")
 
 
 if __name__ == '__main__':
