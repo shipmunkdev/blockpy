fd92c4ffb09bd9a01351b5921c5ce9004b84a3d9
Sein Tun <contact@seintun.dev>
2023-03-15 17:53:00 -0700

    Establish GET endpoint for the Blockchain

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
 
+from backend.blockchain.blockchain import Blockchain
+
 app = Flask(__name__)
+blockchain = Blockchain()
+
+for i in range(3):
+    blockchain.add_block(i)
 
 @app.route('/')
 def test():
     return 'Welcome to the blockchain!'
+
+@app.route('/blockchain')
+def route_blockchain():
+    return blockchain.__repr__()
 
 app.run(port=5000)
